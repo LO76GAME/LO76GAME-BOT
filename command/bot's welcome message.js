0bba03c776e68d47a53265910ac2716172188536
LO76GAME <52280161+LO76GAME@users.noreply.github.com>
2020-01-12 17:09:11 +0100

    Update bot's welcome message.js

--- command/bot's welcome message.js
+++ command/bot's welcome message.js
@@ -2,6 +2,6 @@ bot.on('guildCreate', guild => {
   guild.createChannel("INFOS", 'text').then(channelc => {
     channelc.setTopic("HELLO !")
     
-channelc.send("thanks for adding me ! type `;help` to see my commands !\n type `;setup` to turn on moderation topic !")
+channelc.send("thanks for adding me ! Écrit `;help` pour voir mes commandes !\n type `;setup` to turn on moderation topic (FR&EN u.s.) !\n**NEW**I'm availabe in english u.s. by typing `;;` + command, more with ;;help.")
   })
 });
